2adcfeafe38d360dc09f58a70b09866e8af7db7f
Ayar-Suresh <ahir385350@gmail.com>
2026-06-21 06:27:57 -0700

    feat: configure basePath and add GitHub Pages deployment workflow

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     unoptimized: true,
   },
   trailingSlash: true,
+  // If your GitHub Pages URL is https://<username>.github.io/<repository-name>/,
+  // uncomment the line below and set it to your repository name:
+  basePath: '/AuraDent-Studio',
 };
 
 module.exports = nextConfig;
